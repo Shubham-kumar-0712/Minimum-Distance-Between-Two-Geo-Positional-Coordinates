07b1b0405837b266fc2f8c520626fefd4554f757
Shubham Kumar <123174703+Shubham-kumar-0712@users.noreply.github.com>
2023-01-23 09:49:18 +0530

    Update xyz.py

--- xyz.py
+++ xyz.py
@@ -1,3 +1,5 @@
+# Agenda is to map the content of  df1 with df2 such that geo position distance of df1 is least with the index of df2
+
 import pandas as pd
 import haversine as hs
 import mysql.connector
@@ -15,14 +17,14 @@ data=[]
 for i in range(len(df2)):
     a=(df2.loc[i,"latitude"],df2.loc[i,"longitude"])
     for j in range(len(df1)):
-        b=(df1.loc[j,"latitude"],df1.loc[j,"longitude"])
+        b=(df1.loc[j,"latitude"],df1.loc[j,"longitude"]) #calculating distance for every element of df1
         c=hs.haversine(a,b)
         d=[df2.loc[i,"Project_id"],df2.loc[i,"Project_name"],c,df1.loc[j,"cluster"],df1.loc[j,"plant_id"],df1.loc[j,"name"]]
         data.append(d)
 
 
-df = pd.DataFrame(data, columns=['Project_id','Project_name','distance','cluster','Direct_access_plant','direct_access_plant_name']) 
-df1 = df.loc[df.groupby('Project_id').distance.idxmin()]
+df = pd.DataFrame(data, columns=['Project_id','Project_name','distance','cluster','Direct_access_plant','direct_access_plant_name']) #creating new data frame
+df1 = df.loc[df.groupby('Project_id').distance.idxmin()] # Finding the row which has minimum distance grouping by project id
 df1.to_csv('mapped.csv')
 print(df1)
      
